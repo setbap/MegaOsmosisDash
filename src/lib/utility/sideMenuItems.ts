ff36ceac707ae85c10effbecf08e088f0e775f4d
setbap <ebr.sina@outlook.com>
2023-01-06 06:47:29 +0330

    show defi in menu

--- src/lib/utility/sideMenuItems.ts
+++ src/lib/utility/sideMenuItems.ts
@@ -7,7 +7,7 @@ export default [
   { name: "Transactions", path: "/", icon: BiTransferAlt },
   { name: "Staking", path: "/staking", icon: GiProfit },
   { name: "Stablecoins", path: "/stablecoins", icon: BiDollar },
-  // { name: "Defi", path: "/defi", icon: AiOutlineSwap },
+  { name: "Defi", path: "/defi", icon: AiOutlineSwap },
   { name: "Contract Growth", path: "/contract-growth", icon: BsCodeSquare },
   { name: "Airdrop", path: "/airdrop", icon: GiNightSky },
   { name: "Supply", path: "/supply", icon: GiMeepleCircle },
